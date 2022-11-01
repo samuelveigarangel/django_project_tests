2c13700c30e02805c6b597dff788dca74c25feef
Samuel V <samuelveiga@edu.unifor.br>
2022-11-01 14:52:28 -0300

    test fixed

--- accounts/tests.py
+++ accounts/tests.py
@@ -8,3 +8,19 @@ class CustomUserTest(TestCase):
         user = User.objects.create_user(
             username="samuel", email="samuel@email.com", password="admin"
         )
+        self.assertEqual(user.username, "samuel")
+        self.assertEqual(user.email, "samuel@email.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="samueladmin", email="samueladmin@email.com", password="admin"
+        )
+        self.assertEqual(admin_user.username, "samueladmin")
+        self.assertEqual(admin_user.email, "samueladmin@email.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
