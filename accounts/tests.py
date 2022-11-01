583fafe6ce64494d09809966b332ed45bbc76bb9
Samuel V <samuelveiga@edu.unifor.br>
2022-11-01 14:40:05 -0300

    first commit

---
+++ accounts/tests.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class CustomUserTest(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="samuel", email="samuel@email.com", password="admin"
+        )
